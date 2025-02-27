e82cac8968421a23d8df77dde5d9fc0dba8f67e9
zloimas <zloidinamit@gmail.com>
2025-02-27 23:47:58 +0300

    Добавьте файлы проекта.

---
+++ shutdown/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace shutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(24, 187);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "выключить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(24, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 13);
+            this.textBox1.TabIndex = 2;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectionMode", global::shut.Properties.Settings.Default, "_te", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "секунды",
+            "минуты",
+            "часы"});
+            this.checkedListBox1.Location = new System.Drawing.Point(24, 73);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.SelectionMode = global::shut.Properties.Settings.Default._te;
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 45);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(196, 324);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "пк выключатель";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ shutdown/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace shutdown
+{
+    public partial class Form1 : Form
+    {
+        public int a;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Int32.Parse(textBox1.Text);
+            }
+            finally
+            {
+                if (checkedListBox1.SelectedIndex == 0)
+                {
+                    System.Diagnostics.Process.Start("cmd.exe", "/c shutdown /s /t " + a);
+                }
+                if (checkedListBox1.SelectedIndex == 1)
+                {
+                    System.Diagnostics.Process.Start("cmd.exe", "/c shutdown /s /t " + a * 60);
+                }
+                if (checkedListBox1.SelectedIndex == 2)
+                {
+                    System.Diagnostics.Process.Start("cmd.exe", "/c shutdown /s /t " + a * 3600);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
